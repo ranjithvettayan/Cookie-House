f57939449e75282df89f1fc4ceea776ecd0a0032
ranjithvettayan <40551762+ranjithvettayan@users.noreply.github.com>
2023-12-24 03:02:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const filterButtons = document.querySelectorAll('.menu-filter button');
+const menuItems = document.querySelectorAll('.menu-item');
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const filterValue = button.getAttribute('data-filter');
+    filterItems(filterValue);
+  });
+});
+
+function filterItems(filterValue) {
+  menuItems.forEach(item => {
+    if (filterValue === '*') {
+      item.style.display = 'flex'; // Show all items
+    } else {
+      item.style.display = item.classList.contains(filterValue) ? 'flex' : 'none';
+    }
+  });
+}
